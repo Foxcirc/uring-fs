237ffbbc803dea945a0415e401bb5c87f1f4e2c2
foxcirc <whyfoxcirc@gmx.de>
2023-12-09 17:59:21 +0100

    got basic test working

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,18 @@
 //! This library will spawn one thread that waits for io-uring completions and notifies the waker.
 //! This is nesessary to remain compatible with every runtime.
 
-use std::{fmt, error, io, thread, sync::{Arc, Mutex}, fs, os::fd::AsRawFd, task, future, pin::Pin, marker::PhantomData};
+use std::{fmt, error, io, thread, sync::{Arc, Mutex, atomic}, fs, os::fd::AsRawFd, task, future, pin::Pin, marker::PhantomData, time};
 use private_io_state::IoState;
 
 mod private_io_state {
 
-    use std::sync::Mutex;
+    use std::sync::{Mutex, atomic};
 
     pub struct IoState {
         io_uring: io_uring::IoUring,
         sq_lock: Mutex<()>,
         cq_lock: Mutex<()>,
+        pub in_flight: atomic::AtomicUsize,
     }
 
     impl IoState {
@@ -21,7 +22,8 @@ mod private_io_state {
             Self {
                 io_uring,
                 sq_lock: Mutex::new(()),
-                cq_lock: Mutex::new(())
+                cq_lock: Mutex::new(()),
+                in_flight: atomic::AtomicUsize::new(0),
             }
         }
         pub fn submitter(&self) -> io_uring::Submitter {
@@ -72,14 +74,15 @@ impl<'a> future::Future for Completion<'a> {
 struct CompletionState {
     pub waker: Option<task::Waker>,
     pub result: Option<i32>,
+    pub exit_notification: bool,
 }
 
 pub struct QueueFull;
 impl fmt::Debug for QueueFull {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { write!(f, "QueueFull: the io-uring submission queue is full") }
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { write!(f, "the io-uring submission queue is full") }
 }
 impl fmt::Display for QueueFull {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { write!(f, "QueueFull: the io-uring submission queue is full") }
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { write!(f, "the io-uring submission queue is full") }
 }
 impl error::Error for QueueFull {}
 
@@ -106,14 +109,18 @@ impl IoUring {
                     Ok(..) => (),
                     Err(err) => panic!("reaper thread encountered an error: {}", err)
                 }
-                shared_clone.with_completion(|completion| {
+                let exit = shared_clone.with_completion(|completion| {
+                    let mut exit = false;
                     for event in completion {
+                        shared_clone.in_flight.fetch_sub(1, atomic::Ordering::Relaxed);
                         let data = unsafe { Arc::from_raw(event.user_data() as *mut Mutex<CompletionState>) };
                         let mut guard = data.lock().expect("data lock poisoned");
                         guard.result = Some(event.result());
-                        drop(guard);
+                        if guard.exit_notification { exit = true }
                     }
-                })
+                    exit
+                });
+                if exit { break }
             }
         });
 
@@ -124,24 +131,24 @@ impl IoUring {
 
     }
 
-    pub fn read<'b>(&self, file: fs::File, buf: &'b mut [u8]) -> Result<Completion<'b>, QueueFull> {
+    fn submit<'b>(&self, entry: io_uring::squeue::Entry) -> io::Result<Completion<'b>> {
 
         let state = Arc::new(Mutex::new(CompletionState {
             waker: None,
             result: None,
+            exit_notification: false,
         }));
-        let data = Arc::clone(&state);
 
-        let op = io_uring::opcode::Read::new(
-            io_uring::types::Fd(file.as_raw_fd()),
-            buf.as_mut_ptr(),
-            buf.len() as u32
-        ).build().user_data(Arc::into_raw(data) as u64);
+        let data = Arc::clone(&state);
+        let prepared_entry = entry.user_data(Arc::into_raw(data) as u64);
 
         self.shared.with_submission(|mut submission| {
             // SAFETY: `op` will be valid
-            unsafe { submission.push(&op) }
-        }).map_err(|_| QueueFull)?;
+            unsafe { submission.push(&prepared_entry) }
+        }).map_err(|_| io::Error::new(io::ErrorKind::Other, QueueFull))?;
+
+        self.shared.in_flight.fetch_add(1, atomic::Ordering::Relaxed);
+        self.shared.submitter().submit()?;
 
         Ok(Completion {
             state,
@@ -150,16 +157,60 @@ impl IoUring {
 
     }
 
-    fn join(&mut self) {
-        self.reaper.take().unwrap().join().unwrap();
+    pub fn read<'b>(&self, file: fs::File, buf: &'b mut [u8]) -> io::Result<Completion<'b>> {
+
+        let op = io_uring::opcode::Read::new(
+            io_uring::types::Fd(file.as_raw_fd()),
+            buf.as_mut_ptr(),
+            buf.len() as u32
+        ).build();
+
+        self.submit(op)
+
+    }
+
+    fn kill_reaper(&self) {
+
+        let data = Arc::new(Mutex::new(CompletionState {
+            waker: None,
+            result: None,
+            exit_notification: true,
+        }));
+
+        let op = io_uring::opcode::Timeout::new(
+            &io_uring::types::Timespec::from(time::Duration::ZERO)
+        ).build().user_data(Arc::into_raw(data) as u64);
+
+        self.shared.with_submission(|mut submission| {
+            // SAFETY: `op` will be valid
+            unsafe { submission.push(&op) }
+        }).unwrap();
+
+        self.shared.in_flight.fetch_add(1, atomic::Ordering::Relaxed);
+
+        self.shared.submitter().submit().expect("todo");
+
+    }
+
+    /// How many I/O operations are currently being processed.
+    pub fn in_flight(&self) -> usize {
+        self.shared.in_flight.load(atomic::Ordering::Relaxed)
     }
     
 }
 
 impl Drop for IoUring {
     fn drop(&mut self) {
-        self.join();
+        self.kill_reaper();
+        self.reaper.take().unwrap().join().unwrap();
     }
+}
+
+/// Determines if this [`io::Error`] signals that the io-uring submission queue is full.
+/// If the queue seems so be full regularely the queue size should be increased.
+/// This is a possible error returned by any function that queues a new IO operation.
+pub fn is_queue_full(error: io::Error) -> bool {
+    error.get_ref().map(|inner| inner.downcast_ref::<QueueFull>().is_some()).unwrap_or(false)
 }
 
 #[cfg(test)]
@@ -172,7 +223,10 @@ mod tests {
         let io = crate::IoUring::new().unwrap();
         let file = fs::File::open("src/foo.txt").unwrap();
         let mut buf = [0; 1024];
-        io.read(file, &mut buf);
+        io.read(file, &mut buf).unwrap();
+        std::thread::sleep_ms(1000);
+        println!("buf = {:?}", buf);
+        println!("{}", String::from_utf8_lossy(&buf))
     }
 
 }
